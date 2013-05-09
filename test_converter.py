674c368efc17b79a8a4f90a825e13091e1042338
Lim Ah Seng <lim.ahseng@dhs.sg>
2013-05-09 13:51:42 +0800

    add unit test template

---
+++ test_converter.py
@@ -0,0 +1,20 @@
+# Number bases converter unit testing
+
+from converter import *
+import unittest
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """ initialization, run prior to each test """
+
+    def test_(self):
+        """ test  conversion """
+
+    def tearDown(self):
+        """ clean up, run after each test """
+          
+
+# main
+if __name__ == "__main__":
+    unittest.main(exit=False)
